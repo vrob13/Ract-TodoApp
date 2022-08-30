5142a22eb5ea49621b28741dc208e15a2ad494d1
vrob13 <robinsonvegas@gmail.com>
2022-08-30 12:26:46 +0200

    Updates

---
+++ precache-manifest.393aa0f6113188ff5d50fb55532b639f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "626c9437f2cdcb789086",
+    "url": "/React-TodoApp/static/css/main.940b2cff.chunk.css"
+  },
+  {
+    "revision": "626c9437f2cdcb789086",
+    "url": "/React-TodoApp/static/js/main.626c9437.chunk.js"
+  },
+  {
+    "revision": "6105b0dfdfbec73d7dcc",
+    "url": "/React-TodoApp/static/js/1.6105b0df.chunk.js"
+  },
+  {
+    "revision": "ccc8f01bfbd79eb985d4",
+    "url": "/React-TodoApp/static/js/runtime~main.ccc8f01b.js"
+  },
+  {
+    "revision": "bc66f5ac51b078005ba207d9c404bf9d",
+    "url": "/React-TodoApp/index.html"
+  }
+];
